944fbcb7a119eb3dae7c2486a9b87c4bedd18d6c
viivLgr <viiv_lgr@163.com>
2017-02-10 17:05:42 +0800

    commit

--- static/script/pages/search.js
+++ static/script/pages/search.js
@@ -3,36 +3,36 @@ if(screenHeight<480){
 	screenHeight = 480;
 }
 new Vue({
-		el: '#search',
-		data: {
-			search:[],
-			empty: false,
-			screenHeight:{
-				height: (screenHeight-44) + 'px'
-			},
-			listHeight:{
-				height: (screenHeight-99) + 'px'
-			}
+	el: '#search',
+	data: {
+		search:[],
+		empty: false,
+		screenHeight:{
+			height: (screenHeight-44) + 'px'
 		},
-		methods:{
-			doSearch:function(e){
-				var keyword = $('#searchValue').val();
-				console.log(keyword);
-				var _this = this;
-				$.get('/ajax/search',{
-					keyword: keyword
-				}, function(data) {
-					_this.search = data.items;
-					console.log(data.items)
-					if(_this.search.length == 0){
-						_this.empty = true;
-					}else{
-						_this.empty = false;
-					}
-				},'json');
-			},
-			go:function(book_id){
-				return '/book?id='+book_id;
-			},
+		listHeight:{
+			height: (screenHeight-99) + 'px'
 		}
-	})
+	},
+	methods:{
+		doSearch:function(e){
+			var keyword = $('#searchValue').val();
+			console.log(keyword);
+			var _this = this;
+			$.get('/ajax/search',{
+				keyword: keyword
+			}, function(data) {
+				_this.search = data.items;
+				console.log(data.items)
+				if(_this.search.length == 0){
+					_this.empty = true;
+				}else{
+					_this.empty = false;
+				}
+			},'json');
+		},
+		go:function(book_id){
+			return '/book?id='+book_id;
+		},
+	}
+});
